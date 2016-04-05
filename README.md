cc6773032df7ccb6411a8bf39c553b3cfb67d9a7
zethra <jediben97@gmail.com>
2016-04-05 13:07:46 -0400

    Added callback and wait parameters

--- README.md
+++ README.md
@@ -1,61 +1,72 @@
 # Slic3r Web Interface
 This is and RESTful api for [Slic3r](http://slic3r.org)
 
-##Install
+## Install
  - Install golang
  - Run `go get github.com/zethra/slic3rServer`
  - Run slic3r server binary
  - Make sure the slic3r binary is in your path
 
-##API
-####Slice file
-
-To slice a file send a multipart post request to /slice with the stl file included.  The server will then return the url of the gcode file, which can be downloaded.  Note the http server will not respond until slic3r is done, so make sure you client does not time out.  Additional any other form data included will be used as arguments when running slic3r.  
-
+## API
+### Slice file
+To slice a file send a multipart post request to /slice with the stl file included.  The server will then return the url of the gcode file, which can be downloaded.  
+#### Other Parameters
+ - wait - if set to true server will wait until slic3r is done to respond
+ - callback - if set, server will send a post request containing to gcode file url to the url provided
+ - Any other parameters set will be used as parameters for slic3r
+#### Sample HTTP Request
 <pre>
 POST /slice HTTP/1.1
-Host: example.com
-Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryDeC2E3iWbTv1PwMC
+Host: localhost:8080
+Cache-Control: no-cache
+Postman-Token: da439a4f-572d-5642-4ebd-80d765450dd8
+Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW
 
-------WebKitFormBoundaryDeC2E3iWbTv1PwMC
-Content-Disposition: form-data; name="file"; filename="test.stl"
-Content-Type: application/octet-stream
+----WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="file"; filename=""
+Content-Type: 
 
-STL FILE CONTENT GOES HERE
 
-------WebKitFormBoundaryDeC2E3iWbTv1PwMC
-Content-Disposition: form-data; name="SLIC3R ARG NAME"
+----WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="repair"
 
-"SLIC3R ARG VALUE"
-------WebKitFormBoundaryDeC2E3iWbTv1PwMC--
+
+----WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="layer-height"
+
+0.2
+----WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="wait"
+
+true
+----WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="callback"
+
+http://localhost:8081/callback
+----WebKitFormBoundary7MA4YWxkTrZu0gW
 </pre>
+Resulting command `slic3r stl/test.stl --repair --layer-height 0.2 --output gcode/test.gcode`
 
-####Get file list
-
-stl
-
+### Get a list of file son the server
+Send a get request to /stl or /gcode
+#### Sample HTTP requests
 <pre>
 GET /stl HTTP/1.1
 Host: example.com
 </pre>
-
-gcode
 
 <pre>
 GET /gcode HTTP/1.1
 Host: example.com
 </pre>
 
-####Dowload sliced files
-
-stl
-
+#### Download files
+Send a get request to /stl/filename.stl or /gcode/filename.gcode
+#### Sample HTTP requests
 <pre>
 GET /stl/filename.stl HTTP/1.1
 Host: example.com
 </pre>
-
-gcode
 
 <pre>
 GET /gcode/filename.gcode HTTP/1.1
--- main.go
+++ main.go
@@ -1,147 +1,170 @@
 package main
 
 import (
-    "os"
-    "log"
-    "net/http"
-    "io"
-    "sync"
-    "strings"
-    "os/exec"
-    "fmt"
-    "encoding/xml"
-    "io/ioutil"
-    "encoding/json"
+	"os"
+	"log"
+	"net/http"
+	"io"
+	"sync"
+	"strings"
+	"os/exec"
+	"fmt"
+	"encoding/xml"
+	"io/ioutil"
+	"encoding/json"
+	"bytes"
 )
 
 type Config struct {
-    Port int
-    Slic3rPath string
+	Port       int
+	Slic3rPath string
 }
 
 var config = Config{}
 
 func main() {
-    //Generate Directories
-    if _, err := os.Stat("stl"); os.IsNotExist(err) {
-        log.Println("Making STL Directory")
-        os.Mkdir("stl", 0777)
-    }
-    if _, err := os.Stat("gcode"); os.IsNotExist(err) {
-        log.Println("Making Gcode Directory")
-        os.Mkdir("gcode", 0777)
-    }
-    //Create config file if does not exist
-    if _, err := os.Stat("config.xml"); os.IsNotExist(err) {
-        log.Println("Making config")
-        config.Port = 8080
-        config.Slic3rPath = "slic3r"
-        xml, err := xml.MarshalIndent(config, "", "    ")
-        if (err != nil) {
-            panic(err)
-            return
-        }
-        err = ioutil.WriteFile("config.xml", xml, 0666)
-        if (err != nil) {
-            panic(err)
-            return
-        }
-    } else {
-        //Read config file if does exist
-        data, err := ioutil.ReadFile("config.xml")
-        if (err != nil) {
-            panic(err)
-            return
-        }
-        if (string(data) == "") {
-            return
-        }
-        err = xml.Unmarshal(data, &config)
-        if (err != nil) {
-            panic(err)
-            return
-        }
-    }
-
-    //Start HTTP server
-    http.HandleFunc("/slice", sliceHandler)
-    http.Handle("/gcode/", http.StripPrefix("/gcode/", http.FileServer(http.Dir("gcode"))))
-    http.Handle("/stl/", http.StripPrefix("/stl/", http.FileServer(http.Dir("stl"))))
-    http.HandleFunc("/gcode", FileListHandler)
-    http.HandleFunc("/stl", FileListHandler)
-    log.Printf("HTTP server starting on port :%d\n", config.Port)
-    http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+	//Generate Directories
+	if _, err := os.Stat("stl"); os.IsNotExist(err) {
+		log.Println("Making STL Directory")
+		os.Mkdir("stl", 0777)
+	}
+	if _, err := os.Stat("gcode"); os.IsNotExist(err) {
+		log.Println("Making Gcode Directory")
+		os.Mkdir("gcode", 0777)
+	}
+	//Create config file if does not exist
+	if _, err := os.Stat("config.xml"); os.IsNotExist(err) {
+		log.Println("Making config")
+		config.Port = 8080
+		config.Slic3rPath = "slic3r"
+		xml, err := xml.MarshalIndent(config, "", "    ")
+		if (err != nil) {
+			panic(err)
+			return
+		}
+		err = ioutil.WriteFile("config.xml", xml, 0666)
+		if (err != nil) {
+			panic(err)
+			return
+		}
+	} else {
+		//Read config file if does exist
+		data, err := ioutil.ReadFile("config.xml")
+		if (err != nil) {
+			panic(err)
+			return
+		}
+		if (string(data) == "") {
+			return
+		}
+		err = xml.Unmarshal(data, &config)
+		if (err != nil) {
+			panic(err)
+			return
+		}
+	}
+	//Start HTTP server
+	http.HandleFunc("/slice", sliceHandler)
+	http.Handle("/gcode/", http.StripPrefix("/gcode/", http.FileServer(http.Dir("gcode"))))
+	http.Handle("/stl/", http.StripPrefix("/stl/", http.FileServer(http.Dir("stl"))))
+	http.HandleFunc("/gcode", FileListHandler)
+	http.HandleFunc("/stl", FileListHandler)
+	log.Printf("HTTP server starting on port :%d\n", config.Port)
+	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
 }
 
-func FileListHandler(writer http.ResponseWriter, request *http.Request)  {
-    files, err := ioutil.ReadDir("." + request.URL.String())
-    if(err != nil) {
-        http.Error(writer, err.Error(), 500)
-        return
-    }
-    var fileList []string
-    for _, file := range files {
-        fileList = append(fileList, file.Name())
-    }
-    data, err := json.MarshalIndent(fileList, "", "    ")
-    writer.Write(data)
+func FileListHandler(writer http.ResponseWriter, request *http.Request) {
+	files, err := ioutil.ReadDir("." + request.URL.String())
+	if (err != nil) {
+		http.Error(writer, err.Error(), 500)
+		return
+	}
+	var fileList []string
+	for _, file := range files {
+		fileList = append(fileList, file.Name())
+	}
+	data, err := json.MarshalIndent(fileList, "", "    ")
+	writer.Write(data)
 }
 
 func sliceHandler(writer http.ResponseWriter, request *http.Request) {
-    //Reject request if it is not a POST request
-    if(request.Method != "POST") {
-        http.Error(writer, "Request is not a POST request", 400)
-        return
-    }
-    //Get slic3r args
-    request.ParseMultipartForm(32 << 20)
-    var otherArgs string
+	//Reject request if it is not a POST request
+	if (request.Method != "POST") {
+		http.Error(writer, "Request is not a POST request", 400)
+		return
+	}
+	//Get slic3r args
+	request.ParseMultipartForm(32 << 20)
+	var otherArgs, callback string
+	var wait bool
 	for key, value := range request.Form {
-		if(len(value) > 0) {
-			otherArgs += fmt.Sprintf(" --%s %s", key, value[0])
+		if (key == "callback" && len(value) > 0) {
+			callback = value[0]
+		} else if (key == "wait" && len(value) > 0 && value[0] == "true") {
+			wait = true
 		} else {
-			otherArgs += fmt.Sprintf(" --%s", key)
+			if (len(value) > 0) {
+				otherArgs += fmt.Sprintf(" --%s %s", key, value[0])
+			} else {
+				otherArgs += fmt.Sprintf(" --%s", key)
+			}
 		}
 	}
-    //Get STL file
-    tmpFile, header, err := request.FormFile("file")
-    if err != nil {
-        log.Println(err)
-        http.Error(writer, "Could not parse file form request", 400)
-        return
-    }
-    defer tmpFile.Close()
-    log.Println(header.Header)
-    fileName := header.Filename[:(len(header.Filename) - 4)]
-    file, err := os.OpenFile("stl/" + header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-    if err != nil {
-        log.Println(err)
-        http.Error(writer, "Could not open file: stl/" + header.Filename, 500)
-        return
-    }
-    io.Copy(file, tmpFile)
-    file.Close()
-    //Run slic3r with STL file and args
-    args := fmt.Sprintf(" stl/%s.stl %s --output gcode/%s.gcode", fileName, otherArgs, fileName)
-    wg := new(sync.WaitGroup)
-    wg.Add(1)
-    go exe_cmd(config.Slic3rPath + args, wg)
-    wg.Wait()
-    //Return location of gcode file
-    writer.Write([]byte("/gcode/" + fileName + ".gcode"))
+	//Get STL file
+	tmpFile, header, err := request.FormFile("file")
+	if err != nil {
+		log.Println(err)
+		http.Error(writer, "Could not parse file form request", 400)
+		return
+	}
+	defer tmpFile.Close()
+	log.Println(header.Header)
+	fileName := header.Filename[:(len(header.Filename) - 4)]
+	file, err := os.OpenFile("stl/" + header.Filename, os.O_WRONLY | os.O_CREATE, 0666)
+	if err != nil {
+		log.Println(err)
+		http.Error(writer, "Could not open file: stl/" + header.Filename, 500)
+		return
+	}
+	io.Copy(file, tmpFile)
+	file.Close()
+	//Run slic3r with STL file and args
+	args := fmt.Sprintf(" stl/%s.stl %s --output gcode/%s.gcode", fileName, otherArgs, fileName)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go exe_cmd(config.Slic3rPath + args, wg)
+	if (wait) {
+		wg.Wait()
+	}
+	//Return location of gcode file
+	writer.Write([]byte("/gcode/" + fileName + ".gcode"))
+	go func() {
+		if (callback != "") {
+			wg.Wait()
+			req, err := http.NewRequest("POST", callback, bytes.NewBuffer([]byte("/gcode/" + fileName + ".gcode")))
+			if (err != nil) {
+				log.Println(err)
+			}
+			client := &http.Client{}
+			_, err = client.Do(req)
+			if (err != nil) {
+				log.Println(err)
+			}
+		}
+	}()
 }
 
 func exe_cmd(cmd string, wg *sync.WaitGroup) {
-    log.Println("command is: ", cmd, "\n")
-    parts := strings.Fields(cmd)
-    head := parts[0]
-    parts = parts[1:len(parts)]
+	log.Println("command is: ", cmd)
+	parts := strings.Fields(cmd)
+	head := parts[0]
+	parts = parts[1:len(parts)]
 
-    out, err := exec.Command(head, parts...).Output()
-    if err != nil {
-        log.Printf("%s\n", err)
-    }
-    log.Printf("%s\n", out)
-    wg.Done()
+	out, err := exec.Command(head, parts...).Output()
+	if err != nil {
+		log.Printf("%s\n", err)
+	}
+	log.Printf("%s\n", out)
+	wg.Done()
 }
 
