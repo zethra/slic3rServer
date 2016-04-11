47bc5224a89ea49a24e15cfdcbcb4f18312fe1c2
zethra <jediben97@gmail.com>
2016-04-11 13:15:04 -0400

    Removed old build scripts

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mkdir bin
-set GOPATH=%cd%
-go build -o %GOPATH%\bin\slic3rServer.exe %GOPATH%\src\main.go
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir bin
-export GOPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-go build -o $GOPATH/bin/slic3rServer $GOPATH/src/main.go
